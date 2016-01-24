337ba52d8806052e1aaa5ef95828b4d0b831e22b
Jessy Lecomte <jeslec2015@gmail.com>
2016-01-23 19:14:04 -0500

    Create my network library and add app permissions

--- app/src/main/java/com/lecomte/jessy/popularmovies/MovieListActivity.java
+++ app/src/main/java/com/lecomte/jessy/popularmovies/MovieListActivity.java
@@ -4,18 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
+import com.lecomte.jessy.myhttpurlconnectionlib.MyHttpUrlConnectionUtils;
 import com.lecomte.jessy.popularmovies.dummy.DummyContent;
 
 import java.util.List;
@@ -29,6 +29,8 @@ import java.util.List;
  * item details side-by-side using two vertical panes.
  */
 public class MovieListActivity extends AppCompatActivity {
+
+    private static final String TAG = MovieListActivity.class.getSimpleName();
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -65,6 +67,10 @@ public class MovieListActivity extends AppCompatActivity {
             // activity should be in two-pane mode.
             mTwoPane = true;
         }
+
+        // TEST
+        String jsonStr = MyHttpUrlConnectionUtils.downloadWebPage("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1ed96e22fff439407e05fbfbb876aa3b");
+        Log.d(TAG, jsonStr);
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
