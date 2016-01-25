a3800398ee52b867c46df6edc8dacb2006f7809c
Jessy Lecomte <jeslec2015@gmail.com>
2016-01-25 12:08:24 -0500

    Remove all margins so posters are side-by-side
    Change grid manager type: had set it wrongly to staggeredGrid instead of Grid.

--- app/src/main/java/com/lecomte/jessy/popularmovies/MovieListActivity.java
+++ app/src/main/java/com/lecomte/jessy/popularmovies/MovieListActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -123,9 +123,7 @@ public class MovieListActivity extends AppCompatActivity {
 
         //
         int columnCount = 2; //getResources().getInteger(R.integer.cardview_list_columns);
-        StaggeredGridLayoutManager sglm =
-                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(sglm);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, columnCount));
 
         //recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
     }
